5f99aa17a8406d1ca76a32850b97564f3ecb9581
Cristina Huertas <120569275+CrisStudyZone@users.noreply.github.com>
2024-04-09 16:16:24 -0300

    Pantalla de farmacia de turno falta logica para abrir la llamada cuando se hace click en el numero de telefono y para ir a google maps cuando oprimo la direccion o el icono azul, hay una lista precargada para pruebas, hay que vincular con la lista que entrega goole maps

---
+++ app/src/main/java/com/example/c17_129_kotlin/data/Pharmacy.kt
@@ -0,0 +1,8 @@
+package com.example.c17_129_kotlin.data
+
+data class Pharmacy(
+    val pharmacyName: String,
+    val pharmacyAddress: String,
+    val pharmacyPhone: String,
+    val open: Boolean
+)
