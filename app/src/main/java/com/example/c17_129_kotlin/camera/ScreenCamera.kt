1c3c075ca1fa24d6ae74b5a135596d72f69f8ab9
SantiG <santigime972@gmail.com>
2024-04-25 16:01:38 -0300

    Mejora en la vista de la foto tomada

--- app/src/main/java/com/example/c17_129_kotlin/camera/ScreenCamera.kt
+++ app/src/main/java/com/example/c17_129_kotlin/camera/ScreenCamera.kt
@@ -4,14 +4,10 @@ import android.net.Uri
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -22,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,12 +26,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
-import coil.compose.rememberImagePainter
+import coil.compose.rememberAsyncImagePainter
 import kotlinx.coroutines.launch
 
 @Composable
@@ -89,46 +87,49 @@ fun CameraScreen(
         }
     ){
         if (fotoUri.value != null) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = rememberImagePainter(fotoUri.value!!),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .heightIn(300.dp)
-                        .aspectRatio(1f)
-
-                )
-                Spacer(modifier = Modifier.height(6.dp))
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    FloatingActionButton(
-                        onClick = {
-                            fotoUri.value = null
-                            buttonsVisible = true
-                        }
-                    ) {
-                        Icon(Icons.Default.Close, contentDescription = "Cancelar")
-                    }
-                    FloatingActionButton(
-                        onClick = {
-                            navController.popBackStack()
-                        }
-                    ) {
-                        Icon(Icons.Default.Check, contentDescription = "Aceptar")
-                    }
+            Photo(
+                fotoUri,
+                navController,
+                clickCancel = {
+                    fotoUri.value = null
+                    buttonsVisible = true
                 }
-            }
+            )
         }else{
             CamaraCompose(cameraController, lifecycle, modifier = Modifier.padding(it))
         }
 
+    }
+}
+
+@Composable
+fun Photo(
+    fotoUri: MutableState<Uri?>,
+    navController: NavHostController,
+    clickCancel: () -> Unit
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = rememberAsyncImagePainter(fotoUri.value!!),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            Button(onClick = { clickCancel() }) {
+                Icon(Icons.Default.Close, contentDescription = "Cancelar")
+            }
+            Button(onClick = { navController.popBackStack() }) {
+                Icon(Icons.Default.Check, contentDescription = "Aceptar")
+            }
+        }
     }
 }
